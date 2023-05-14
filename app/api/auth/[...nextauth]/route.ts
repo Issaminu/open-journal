85f53196cdb1c48a545cc3a74c1ed422fc0b9d4c
Issam-Boubcher <boubcher.issam@gmail.com>
2023-05-14 18:44:42 +0100

    Fix for Vercel build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
