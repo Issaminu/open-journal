1e3d0048ddc4b85215f72ff0aa8341fed45c97ae
Issam-Boubcher <boubcher.issam@gmail.com>
2023-05-14 23:09:59 +0100

    Add seeding with fake data using faker-js

--- prisma/migrations/20230514200534_init/migration.sql
+++ prisma/migrations/20230514200534_init/migration.sql
@@ -30,6 +30,7 @@ CREATE TABLE "Article" (
 CREATE TABLE "Category" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
@@ -37,8 +38,8 @@ CREATE TABLE "Category" (
 -- CreateTable
 CREATE TABLE "Comment" (
     "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
     "content" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
     "articleId" INTEGER NOT NULL,
 
     CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
@@ -63,7 +64,13 @@ CREATE INDEX "_ArticleToCategory_B_index" ON "_ArticleToCategory"("B");
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Comment" ADD CONSTRAINT "Comment_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_ArticleToCategory" ADD CONSTRAINT "_ArticleToCategory_A_fkey" FOREIGN KEY ("A") REFERENCES "Article"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ seed/seed.js
@@ -0,0 +1,133 @@
+// This file is a replica of the prisma/seed.ts file.
+// It is created for the sole reason of conforming to the project's requirements.
+// Prisma recommends having the seed file in the prisma folder.
+
+const { PrismaClient } = require("@prisma/client");
+const { fakerEN_US } = require("@faker-js/faker");
+
+const prisma = new PrismaClient();
+const faker = fakerEN_US;
+
+async function main() {
+  try {
+    const userCount = 20;
+    const articleCount = 300;
+    const categoryCount = 300;
+    const commentCount = 500;
+
+    const fakeUsers = generateFakeUsers(userCount);
+
+    await prisma.$transaction([prisma.user.createMany({ data: fakeUsers })]);
+
+    const lastUser = await prisma.user.findFirst({
+      orderBy: {
+        id: "desc",
+      },
+      select: {
+        id: true,
+      },
+    });
+    const lastAuthorId = lastUser ? lastUser.id : 1;
+    const firstAuthorId = lastUser ? lastAuthorId - userCount + 1 : 1;
+
+    const fakeArticles = generateFakeArticles(
+      articleCount,
+      firstAuthorId,
+      lastAuthorId
+    );
+
+    await prisma.$transaction([
+      prisma.article.createMany({ data: fakeArticles }),
+    ]);
+
+    const lastArticle = await prisma.article.findFirst({
+      orderBy: {
+        id: "desc",
+      },
+      select: {
+        id: true,
+      },
+    });
+    const lastArticleId = lastArticle ? lastArticle.id : 1;
+    const firstArticleId = lastArticle ? lastArticleId - articleCount + 1 : 1;
+
+    const fakeCategories = generateFakeCategories(
+      categoryCount,
+      firstAuthorId,
+      lastAuthorId
+    );
+
+    const fakeComments = generateFakeComments(
+      commentCount,
+      firstAuthorId,
+      lastAuthorId,
+      firstArticleId,
+      lastArticleId
+    );
+
+    await prisma.$transaction([
+      prisma.category.createMany({ data: fakeCategories }),
+      prisma.comment.createMany({ data: fakeComments }),
+    ]);
+
+    console.log(
+      `Added ${userCount} users, ${articleCount} articles, ${categoryCount} categories, and ${commentCount} comments.`
+    );
+  } catch (e) {
+    console.error(e);
+    await prisma.$disconnect();
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+main();
+
+function generateFakeUsers(count) {
+  return [...Array(count)].map(() => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    role: "AUTHOR",
+  }));
+}
+
+function generateFakeArticles(count, minAuthorId, maxAuthorId) {
+  return [...Array(count)].map(() => {
+    const title = faker.lorem.sentence();
+    const content = faker.lorem.paragraphs();
+    return {
+      title: title.charAt(0).toUpperCase() + title.slice(1),
+      content: content.charAt(0).toUpperCase() + content.slice(1),
+      isPublished: true,
+      authorId: faker.number.int({ min: minAuthorId, max: maxAuthorId }),
+      image: faker.image.url(),
+    };
+  });
+}
+
+function generateFakeCategories(count, minAuthorId, maxAuthorId) {
+  return [...Array(count)].map(() => {
+    const name = faker.word.noun();
+    return {
+      name: name.charAt(0).toUpperCase() + name.slice(1),
+      authorId: faker.number.int({ min: minAuthorId, max: maxAuthorId }),
+    };
+  });
+}
+
+function generateFakeComments(
+  count,
+  minAuthorId,
+  maxAuthorId,
+  minArticleId,
+  maxArticleId
+) {
+  return [...Array(count)].map(() => {
+    const content = faker.lorem.paragraph();
+    return {
+      content: content.charAt(0).toUpperCase() + content.slice(1),
+      authorId: faker.number.int({ min: minAuthorId, max: maxAuthorId }),
+      articleId: faker.number.int({ min: minArticleId, max: maxArticleId }),
+    };
+  });
+}
