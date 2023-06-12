ecd1364da5d53214f70c0588a9dfad9f79966cab
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2023-06-12 15:48:52 -0700

    Update HomeHeader.tsx

--- components/my-components/HomeHeader.tsx
+++ components/my-components/HomeHeader.tsx
@@ -30,7 +30,7 @@ const HomeHeader = ({ metaData }: { metaData: getMetaDataType }) => {
           />
           <div className="flex flex-col justify-center">
             <h1 className="text-4xl font-bold text-[#ecd7d7]">
-              The ENSET Journal
+              The Open Journal
             </h1>
             <p className="pt-2 text-[#cc9b9c]/80">
               {metaData.articleCount} articles · {metaData.userCount} students
@@ -58,7 +58,7 @@ const HomeHeader = ({ metaData }: { metaData: getMetaDataType }) => {
       </div>
 
       <p className="whitespace-wrap max-w-[50rem] pt-10 text-justify font-hubot text-lg text-[#EED3D3]">
-        The online blog of ENSET Mohammedia. The open platform where students
+        The open blog of the Internet. The platform where anyone
         can freely express themselves, offering a captivating virtual hub for
         intellectual exploration and thought-provoking articles, personal
         narratives, cutting-edge research, and engaging discussions.
